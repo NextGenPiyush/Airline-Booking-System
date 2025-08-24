d2d9d112f633c53f0e66cdefe16a1671fecbc0dc
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-08-24 19:24:39 +0530

    added mew migration that can add a constraint in a column of Airport

--- src/models/airport.js
+++ src/models/airport.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongTo(models.City, {
+        foreignKey: 'cityId',
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE'
+      });
     }
   }
   Airport.init({
