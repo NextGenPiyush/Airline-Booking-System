e1c8e393dabe01a6f1dfe6e486c6e0707a043b88
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-08-26 20:36:43 +0530

    Creating other booking microservice

---
+++ Flight-Booking-Service/src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const{InfoController} = require('../../controllers');
+
+const router = express.Router();
+
+// router.get('/info', (req, res) => {
+//     return res.json({msg: "ok"});
+// });
+router.get('/info', InfoController.info);  // nore cleaner implementation
+
+module.exports = router;
