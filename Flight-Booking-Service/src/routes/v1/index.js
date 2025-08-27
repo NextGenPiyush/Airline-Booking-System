99daa6b69766043e4142ae8a203dfcef4e8cc818
Piyush Goyal <piyushgoyal2789@gmail.com>
2025-08-28 05:15:32 +0530

    Started Booking implementation by creatingBooking transaction

--- Flight-Booking-Service/src/routes/v1/index.js
+++ Flight-Booking-Service/src/routes/v1/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 
 const{InfoController} = require('../../controllers');
 
+const bookingRoutes = require('./booking');
+
 const router = express.Router();
 
 // router.get('/info', (req, res) => {
 //     return res.json({msg: "ok"});
 // });
 router.get('/info', InfoController.info);  // nore cleaner implementation
+
+router.use('/bookings', bookingRoutes);
 
 module.exports = router;
